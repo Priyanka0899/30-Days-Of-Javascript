c5e67eae668d0aed60ff49f4ba548040db15acda
Priyanka Arora <parora0711@gmail.com>
2024-07-22 10:37:48 +0530

    changes commited

---
+++ Day1/datatypes.js
@@ -0,0 +1,30 @@
+/* let name = "Priyanka Arora"
+let isStudent = true;
+let haveAnyWhereDoor = undefined;
+let haveMoney = null;
+
+console.log(typeof(name));
+console.log(typeof(isStudent));
+console.log(typeof(haveAnyWhereDoor));
+console.log(typeof(haveMoney));
+
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+
+let Name = "Priyanka";
+let surname = "Arora";
+let age = 24;
+let weight = 55.1;
+
+//let first_name = "Priyanka";
+//let last_name = "Arora";
+//let marital_status = "Unmarried";
+//let country = "India";
+//let MyAge  = 24; 
+*/
+
+let first_name = "Priyanka", last_name = "Arora", marital_status = "Unmarried", country = "India", MyAge  = 24  ;
+
+
