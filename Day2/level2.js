68c584d3d09d12d4fe61b4b114f021d8c40ff17a
Priyanka Arora <parora0711@gmail.com>
2024-07-24 01:36:21 +0530

    committed

---
+++ Day2/level2.js
@@ -0,0 +1,39 @@
+// console.log("The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another'");
+// console.log("\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"");
+
+
+// const str = '10';
+// const num = 10;
+
+// if (typeof str !== typeof num) {
+//   const convertedStr = Number(str); 
+//   console.log(convertedStr === num); 
+// } else {
+//   console.log(str === num); 
+// }
+
+parseFloat('9.8')===10 ? true: console.log(Math.ceil(9.8));
+
+let py = 'python';
+let jar = "jargon";
+console.log(py.includes('on'), jar.includes('on'));
+
+let s = 'I hope this course is not full of jargon.';
+console.log(s.includes('jargon'));
+
+// const randomNumber = Math.floor(Math.random() * 101);
+// console.log(randomNumber);
+
+
+// const randomNumber = Math.floor(Math.random() * 256);
+// console.log(randomNumber);
+
+
+const rand = Math.floor(Math.random() * 101);
+console.log(rand>50 ? rand : rand+50);
+
+// 10 pending
+// 12 pending
+
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
+
---
+++ Day2/level3.js
@@ -0,0 +1,12 @@
+// const sen = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+// const matches = sen.match(/love/gi);
+// const count = matches ? matches.length : 0;
+// console.log(`love is ${count} times present`);
+
+const sen = 'You cannot end a sentence with because because because is a conjunction';
+const matches = sen.match(/because/gi);
+const count = matches ? matches.length : 0;
+console.log(`because is ${count} times present`);
+
+//3 Pending
+//4 Pending
