58aa1f3cab80c271a418c383260d9411bc3ee83b
Priyanka Arora <parora0711@gmail.com>
2024-07-24 18:53:43 +0530

    committed

--- Day3/level1.js
+++ Day3/level1.js
@@ -1,26 +1,26 @@
-let firstName = 'Priyanka', lastName = 'Arora', country = 'india', city = 'Mansa', age= 24, isMarried= false;
+// let firstName = 'Priyanka', lastName = 'Arora', country = 'india', city = 'Mansa', age= 24, isMarried= false;
 
-console.log(typeof firstName, typeof lastName,typeof country ,typeof city, typeof age, typeof isMarried);
+// console.log(typeof firstName, typeof lastName,typeof country ,typeof city, typeof age, typeof isMarried);
 
-console.log(typeof '10' == typeof 10);
-console.log(parseInt('9.8')== 10);
+// console.log(typeof '10' == typeof 10);
+// console.log(parseInt('9.8')== 10);
 
-console.log(1=='1', true == 1, 0== '0', 1==='1', false ===0, 0==='0');
+// console.log(1=='1', true == 1, 0== '0', 1==='1', false ===0, 0==='0');
 
-console.log(4 > 3, 4 >= 3, 4 < 3, 4 <= 3, 4 == 4, 4 === 4, 4 != 4, 4 !== 4, 4 != '4', 4 == '4', 4 === '4' );
+// console.log(4 > 3, 4 >= 3, 4 < 3, 4 <= 3, 4 == 4, 4 === 4, 4 != 4, 4 !== 4, 4 != '4', 4 == '4', 4 === '4' );
 
-console.log('python'.length !== 'jargon'.length);
+// console.log('python'.length !== 'jargon'.length);
 
-console.log(4 > 3 && 10 < 12, 4 > 3 && 10 > 12, 4 > 3 || 10 < 12, 4 > 3 || 10 > 12, !(4 > 3), !(4 < 3), !(false), !(4 > 3 && 10 < 12), !(4 > 3 && 10 > 12), !(4 === '4'), 'python'.includes('on') && 'jargon'.includes('on'));
+// console.log(4 > 3 && 10 < 12, 4 > 3 && 10 > 12, 4 > 3 || 10 < 12, 4 > 3 || 10 > 12, !(4 > 3), !(4 < 3), !(false), !(4 > 3 && 10 < 12), !(4 > 3 && 10 > 12), !(4 === '4'), 'python'.includes('on') && 'jargon'.includes('on'));
 
-const now = new Date();
-console.log(now.getFullYear());
-console.log(now.getMonth()+1);
-console.log(now.getDate());
-console.log(now.getDay());
-console.log(now.getHours());
-console.log(now.getMinutes());
-console.log(Date.now());
+// const now = new Date();
+// console.log(now.getFullYear());
+// console.log(now.getMonth()+1);
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(Date.now());
 
 
 
---
+++ Day3/level2.js
@@ -0,0 +1,47 @@
+// const base = prompt("Enter the base");
+// const height = prompt('Enter the height');
+// alert(`The area of the triangle is ${0.5*base*height}`);
+
+// const a = prompt("Enter side a");
+// const b = prompt('Enter side b');
+// const c = prompt('Enter side c');
+// alert(`The perimeter of the triangle is  ${+a + +b + +c}`);
+
+// const length = prompt("Enter the length");
+// const width = prompt('Enter the width');
+// alert(`The area of the rectangle is ${length*width}`);
+// alert(`The perimeter of the rectangle is  ${(2*(+length+ +width))}`);
+
+// const radius = prompt("Enter the radius");
+// const PI = 3.14;
+// alert(`The area of the circle is ${PI*(radius*radius)}`);
+// alert(`The circumference of the circle is ${(2*PI*radius)}`);
+
+//5 pending
+
+// const [x1,y1] = [2,2];
+// const [x2,y2] = [6,10];
+// alert(`slope is ${(y2-y1)/(x2-x1)}`);
+
+// console.log('Priyanka'.length > 7 ? 'long': 'short');
+
+// let fName = 'Priyanka';
+// let lName = 'Arora';
+// if(fName.length>lName.length)
+// {
+//     console.log('your first name is longer than last name');
+// }
+// else{
+//     console.log('your first name is shorter than last name');
+// }
+
+// let myAge = 250;
+// let yourAge = 25;
+// console.log(`i am ${myAge-yourAge} older than you `);
+
+const live = 100; 
+console.log(100*365*24*60*60);
+
+const now = new Date();
+console.log(`${now.getFullYear()}-${now.getMonth()+1}-${now.getDay()} ${now.getHours()}:${now.getMinutes()}`);
+console.log(`${now.getFullYear()}/${now.getMonth()+1}/${now.getDay()} ${now.getHours()}:${now.getMinutes()}`);
