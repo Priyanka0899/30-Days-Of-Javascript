b04fbdc94200365b21eac1232f489cd7509775b1
Priyanka Arora <parora0711@gmail.com>
2024-07-28 03:48:37 +0530

    committed

---
+++ Day6/level1.js
@@ -0,0 +1,73 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+
+//   for(let i=0;i<10;i++)
+//   {
+//     console.log(countries[i])
+//   }
+
+//   let i=0;
+//   while(i<10)
+//   {
+//     console.log(countries[i])
+//     i++;
+//   }
+
+//   let i = 0;
+//   do{
+//     console.log(countries[i])
+//     i++;
+//   }while(i<10)
+
+// for(let i=9;i>=0;i--)
+// {
+//     console.log(countries[i])
+// }
+
+// let i=9;
+// while(i>=0)
+// {
+//     console.log(countries[i])
+//     i--;
+// }
+
+// for(let i=0;i<=10;i++)
+// {
+//     console.log(`${i} x ${i} = ${i*i}`);
+// }
+
+// for(let i=0;i<=100;i++)
+// {
+//     if(i%2==0)
+//     {
+//         console.log(i);
+//     }
+// }
+
+// for(let i=0;i<=100;i++)
+//     {
+//         if(i%2!=0)
+//         {
+//             console.log(i);
+//         }
+//     }
+    // let sum = 0;
+    // for(let i=0;i<=100;i++)
+    //     {
+    //         sum=sum+i;
+    //     }
+    //     console.log(sum)
+
+    
+
